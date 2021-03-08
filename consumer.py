3f7455ba8f6aeac555e898cab0c3c5b7c5910953
Dax Thompson <dax.thompson@aggiemail.usu.edu>
2021-03-08 14:07:12 -0700

    Can read files from S3

--- consumer.py
+++ consumer.py
@@ -1,7 +1,21 @@
-import boto3
+import boto3, os
 from sys import argv
+
+def connect(from_bucket, method, to_bucket):
+    try:
+        client = boto3.client('s3')
+        requests = client.list_objects(Bucket=from_bucket)
+        request = boto3.resource('s3').Object(from_bucket, requests['Contents'][0]['Key'])
+        request.download_file('file')
+        request_file = open('file')
+        print(request_file.read())
+        request_file.close()
+        os.remove('file')
+
+    except:
+        print('Unable to connect')
 
 if len(argv) < 4:
     print('Usage: consumer.py bucket_1 method bucket_2')
 else:
-    print('Success')
+    connect(argv[1], argv[2], argv[3])
