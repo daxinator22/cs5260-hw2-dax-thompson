6726db51fa9bc9d9f43f0ab0afff45d714ab0554
Dax Thompson <dax.thompson@aggiemail.usu.edu>
2021-03-08 17:43:06 -0700

    Can now put objects in S3 bucket

--- consumer.py
+++ consumer.py
@@ -1,22 +1,53 @@
 import boto3, os, json
 from sys import argv
 
+def get_object(from_bucket, object_key, file_name):
+    try:
+        request = boto3.resource('s3').Object(from_bucket, object_key)
+        request.download_file(file_name)
+    except:
+        print(f'Error downloading object {object_key} from S3')
+
+def read_object(request_file, file_name):
+    try:
+        request_file = open(file_name)
+        json_object = json.loads(request_file.read())
+        request_file.close()
+        return json_object
+    except:
+        print(f'Error reading JSON object')
+
+
+def put_object(method, to_bucket, json_object, file_name):
+    try:
+        print(f'Connecting to {to_bucket} using {method}')
+        to_resource = boto3.resource(method)
+        del json_object['type']
+        del json_object['requestId']
+        f = open(file_name, 'w')
+        f.write(json_object)
+        f.close()
+        #to_resource.Object(to_bucket, json_object['widgetId']).upload_file(file_name)
+    except:
+        print(f'Error putting JSON object')
+
+
 def connect(from_bucket, method, to_bucket):
+    requests = None
     try:
         from_client = boto3.client('s3')
         requests = from_client.list_objects(Bucket=from_bucket)
-        for request in requests['Contents']:
-            request = boto3.resource('s3').Object(from_bucket, request['Key'])
-            request.download_file('file')
-            request_file = open('file')
-            json_object = json.loads(request_file.read())
-            request_file.close()
-            to_resource = boto3.resource(method)
-            to_resource.Object(to_bucket, json_object['widgetId']).upload_file('file')
-            os.remove('file')
-
     except:
-        print('Unable to connect')
+        print('Error retrieving objects from S3')
+
+    file_name = 'json_file.txt'
+
+    for request in requests['Contents']:
+        get_object(from_bucket, request['Key'], file_name)
+        json = read_object(file_name, file_name)
+        put_object(method, to_bucket, json, file_name)
+        os.remove(file_name)
+
 
 if len(argv) < 4:
     print('Usage: consumer.py bucket_1 method bucket_2')
