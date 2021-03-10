f4f43563c04ea9a953a55696dcb50f493855bb37
Dax Thompson <daxpault@gmail.com>
2021-03-09 21:42:57 -0700

    Merging

--- consumer.py
+++ consumer.py
@@ -1,11 +1,11 @@
-import boto3, os, json
+import boto3, os, json, logging
 from sys import argv
 
 def get_object(from_bucket, object_key, file_name):
     try:
         request = boto3.resource('s3').Object(from_bucket, object_key)
         request.download_file(file_name)
-        request.delete()
+        #request.delete()
     except:
         print(f'Error downloading object {object_key} from S3')
 
@@ -21,7 +21,7 @@ def read_object(request_file, file_name):
 
 def put_object(method, to_bucket, json_object, file_name):
     try:
-        print(f'{json_object["requestId"]} {json_object["type"][:-1]}ing {json_object["widgetId"]} in {to_bucket} using {method}')
+        print(f'{json_object["type"][:-1].capitalize()}ing {json_object["widgetId"]} in {to_bucket} using {method}')
         to_resource = boto3.resource(method)
         if json_object['type'] == 'create':
             del json_object['type']
@@ -29,9 +29,9 @@ def put_object(method, to_bucket, json_object, file_name):
             f = open(file_name, 'w')
             f.write(json.dumps(json_object))
             f.close()
-            to_resource.Object(to_bucket, json_object['widgetId']).upload_file(file_name)
+            to_resource.Object(to_bucket, f'{json_object["owner"]}/{json_object["widgetId"]}').upload_file(file_name)
         elif json_object['type'] == 'delete':
-            to_resource.Object(to_bucket, json_object['widgetId']).delete()
+            to_resource.Object(to_bucket, f'{json_object["widgetId"]}').delete()
     except:
         print(f'Error putting JSON object')
 
@@ -53,7 +53,7 @@ def connect(from_bucket, method, to_bucket):
         os.remove(file_name)
 
 
-if len(argv) < 4:
-    print('Usage: consumer.py bucket_1 method bucket_2')
-else:
-    connect(argv[1], argv[2], argv[3])
+#if len(argv) < 4:
+    #print('Usage: consumer.py bucket_1 method bucket_2')
+#else:
+    #connect(argv[1], argv[2], argv[3])
