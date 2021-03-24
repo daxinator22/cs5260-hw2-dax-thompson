7eafd8575f93260dfeb7715982def4698a8c43c4
Dax Thompson <dax.thompson@aggiemail.usu.edu>
2021-03-24 13:14:11 -0600

    Refactoring client

--- consumer/Client.py
+++ consumer/Client.py
@@ -4,13 +4,19 @@ from botocore.exceptions import UnknownServiceError, ClientError
 
 class Client():
 
-    def __init__(self, from_bucket, method, to_bucket):
+    def __init__(self, queue, method, destination):
         logging.basicConfig(filename='log.log', level=logging.INFO)
-        self.from_bucket = from_bucket
+        self.queue = queue
         self.method = method
-        self.to_bucket = to_bucket
+        self.destination = destination
 
-        self.from_client = boto3.client('s3')
+        self.types = {
+            'create' : self.put_widget,
+            'update' : self.update_widget,
+            'delete' : self.delete_widget,
+        }
+
+        self.queue_client = boto3.client('s3')
         try:
             self.to_client = boto3.client(self.method)
         except UnknownServiceError:
@@ -23,22 +29,30 @@ class Client():
         except KeyError:
             time.sleep(0.1)
             return
+            
 
-        if request.type == 'create':
-            self.put_widget(request.content)
+        self.types[request.type](request.content)
 
     def get_request(self):
         try:
-            request_object = self.from_client.list_objects(Bucket=self.from_bucket, MaxKeys=1)['Contents'][0]
-            request = Request.Request(request_object['Key'], self.from_client.get_object(Bucket=self.from_bucket, Key=request_object['Key'])['Body'].read().decode('utf-8'))
+            request_object = self.queue_client.list_objects(Bucket=self.queue, MaxKeys=1)['Contents'][0]
+            request = Request.Request(request_object['Key'], self.queue_client.get_object(Bucket=self.queue, Key=request_object['Key'])['Body'].read().decode('utf-8'))
             logging.info(f'Processing create request {request.key} at {datetime.datetime.now()}')
             print(f'Processing {request.type} request {request.key} at {datetime.datetime.now()}')
-            self.from_client.delete_object(Bucket=self.from_bucket, Key=request.key)
+            self.queue_client.delete_object(Bucket=self.queue, Key=request.key)
             return request
         except ClientError:
-            raise BucketNotFound(f'{self.from_bucket} does not exist')
+            raise BucketNotFound(f'{self.queue} does not exist')
         except KeyError:
             raise KeyError
+
+    def update_widget(self, content):
+        #TODO: Update Widget
+        return
+
+    def delete_widget(self, content):
+        #TODO: Delete Widget
+        return
 
 
     def put_widget(self, content):
@@ -47,15 +61,15 @@ class Client():
                 widget = S3_Widget.S3_Widget(content)
                 logging.info(f'Processing widget {widget.key} at {datetime.datetime.now()}')
                 print(f'Processing widget {widget.key} at {datetime.datetime.now()}')
-                self.to_client.put_object(Bucket=self.to_bucket, Key=f'{widget.owner}/{widget.key}', Body=bytes(widget.content, 'utf-8'))
+                self.to_client.put_object(Bucket=self.destination, Key=f'{widget.owner}/{widget.key}', Body=bytes(widget.content, 'utf-8'))
             except ClientError:
-                raise BucketNotFound(f'{self.to_bucket} does not exist')
+                raise BucketNotFound(f'{self.destination} does not exist')
 
         elif self.method == 'dynamodb':
             try:
                 widget = DynamoDB_Widget.DynamoDB_Widget(content)
                 logging.info(f'Processing widget {widget.key} at {datetime.datetime.now()}')
                 print(f'Processing widget {widget.key} at {datetime.datetime.now()}')
-                self.to_client.put_item(TableName=self.to_bucket, Item=widget.content)
+                self.to_client.put_item(TableName=self.destination, Item=widget.content)
             except:
-                raise BucketNotFound(f'{self.to_bucket} does not exist')
+                raise BucketNotFound(f'{self.destination} does not exist')
--- consumer/consumer.py
+++ consumer/consumer.py
@@ -2,7 +2,7 @@ from sys import argv
 import Client
 
 if len(argv) < 4:
-    print('Usage: consumer.py from_bucket method to_bucket')
+    print('Usage: consumer.py queue method destination')
 else:
     client = Client.Client(argv[1], argv[2], argv[3])
     while True:
