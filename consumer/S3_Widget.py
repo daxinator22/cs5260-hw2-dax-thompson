2e83ef0348d10ff7d440501aa1e777acdfd91820
Dax Thompson <dax.thompson@aggiemail.usu.edu>
2021-03-28 00:01:11 -0600

    Fixed update request method for S3

--- consumer/S3_Widget.py
+++ consumer/S3_Widget.py
@@ -1,4 +1,5 @@
 import Widget, json
+from dict_recursive_update import recursive_update
 
 class S3_Widget(Widget.Widget):
 
@@ -12,7 +13,7 @@ class S3_Widget(Widget.Widget):
     def create_widget(self, client, destination):
         client.put_object(Bucket=destination, Key=f'{self.owner}/{self.key}', Body=bytes(self.content, 'utf-8'))
 
-    def delete_wiget(self, client, destination):
+    def delete_widget(self, client, destination):
         client.delete_object(Bucket=destination, Key=self.key)
 
     def update_widget(self, client, destination):
@@ -24,15 +25,14 @@ class S3_Widget(Widget.Widget):
             old_widget = json.loads(old_object)
             
             #Updates object
-            old_widget.update(self.content)
+            recursive_update(old_widget, json.loads(self.content))
 
             #Creates new widget
-            self.content = old_widget
+            self.content = json.dumps(old_widget)
             self.create_widget(client, destination)
 
         except client.exceptions.NoSuchKey:
             print(f'Could not find key {self.key}')
 
         except ValueError as e:
-            print(old_widget)
-            print(e)
+            print(f'Old Widget: {old_widget}\nNew Widget: {self.content}')
